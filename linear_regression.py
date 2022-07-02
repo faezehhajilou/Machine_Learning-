e5174c6ddf17ad98630e492e950e55ffd002fbea
faezehhajilou <faezeh.hajilou@yahoo.com>
2022-07-02 14:19:38 +0430

    second commit for linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+# Data Generating - Data Loading
 np.random.seed(2)
 N = 1000
 X = np.random.randn(N, 1)
@@ -23,6 +24,18 @@ for i in range(len(X_train)):
     num += X_train[i] * Y_train[i]
     den += X_train[i] ** 2
 w = num/den
-
 print(w)
 
+# Evaluate the Model
+Y_pre = w[0][0] + w[0][1] * X_test
+plt.plot(X_test, Y_pre, 'r')
+plt.plot(X_test, Y_test, 'b.')
+plt.legend(['Estimated_values', 'Trues_values'])
+# plt.show()
+
+# Evaluation of Error
+error = Y_pre - Y_test
+print(f'mean_error: {np.mean(error)}')
+print(f'std_error: {np.std(error)}')
+plt.plot(error)
+plt.show()
